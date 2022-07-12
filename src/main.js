c9b5ff2e15736e8b0a1b486daa65c1757658dabd
Baskara Febrianto <baskarafebrianto@gmail>
2022-07-12 21:52:07 +0700

    adding typscript

---
+++ src/main.js
@@ -0,0 +1,5 @@
+export { default as paginate } from './paginate';
+export { default as PaginationNav } from './PaginationNav.svelte';
+export { default as LightPaginationNav } from './LightPaginationNav.svelte';
+export { default as DarkPaginationNav } from './DarkPaginationNav.svelte';
+//# sourceMappingURL=main.js.map
--- src/paginate.js
+++ src/paginate.js
@@ -1,7 +1,5 @@
 export default function ({ items, pageSize, currentPage }) {
-  return items
-    .slice(
-      (currentPage - 1) * pageSize,
-      (currentPage - 1) * pageSize + pageSize
-    )
+    return items
+        .slice((currentPage - 1) * pageSize, (currentPage - 1) * pageSize + pageSize);
 }
+//# sourceMappingURL=paginate.js.map
